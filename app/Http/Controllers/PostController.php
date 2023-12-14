8749c0a769e10122dcb916aed9995e36cd541951
kanaraki-creater <KanAraki49@gmail.com>
2023-12-14 10:21:54 +0900

    カリキュラム8-1をやり直しました

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,8 +7,9 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
+    
     public function index(Post $post)
     {
-        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);
+        return $post->get();
     }
 }
