8db746756aa37298ef1e95b60c63778e0931fa60
kanaraki-creater <KanAraki49@gmail.com>
2023-12-14 09:45:43 +0900

    編集しました

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,11 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return $post->get();
+        return view('posts/index')->with(['posts' => $post->get(1)]); 
+    }
+    
+    public function show(Post $post)
+    {
+        dd($post);
     }
 }
